93a32fd21571cbe2967cccff4789dc8adf5959cf
Berfin Altunbey <baltunbey98@gmail.com>
2023-01-31 02:20:14 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Card from "./components/Card"
+import data from "./data"
+
+export default function App() {
+    const cards = data.map(card => {
+        return (
+            <Card 
+            key={card.id}
+            {...card}
+        />
+        )
+    })
+    return (
+        <div>
+        <Navbar />
+        {cards}
+        </div>
+    )
+}
---
+++ data.js
@@ -0,0 +1,29 @@
+export default [
+    {   id:1, 
+        title: "Pamukkale",
+        location: "Turkey",
+        googleMapsUrl: "https://goo.gl/maps/GYqzc11go8VgKTxm6",
+        startDate: "4 April, 2022",
+        endDate: "5 April, 2022",
+        description: "The stunning white calcium pools, which cling to the side of a ridge, have long been one of the most famous picture postcard views of Türkiye. Pamukkale, literally meaning “cotton castle”, is also the site of the ancient city of Hierapolis of which there are many interesting ruins, and is a very popular destination for a short visit.",
+        imageUrl: "https://images.unsplash.com/photo-1637000019085-be6dd1d98715?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {   id:2,
+        title: "Antalya",
+        location: "Turkey",
+        googleMapsUrl: "https://goo.gl/maps/EjruZGDFixeqDibV7",
+        startDate: "8 Aug, 2022",
+        endDate: "14 Aug, 2022",
+        description: "The Antalya Region, offering all the mysticism of past in our day, is now called the 'Turkish Riviera' due to its archaeological and natural beauties. Antalya is the place where sea, sun, history and nature constitute a perfect harmony and which also includes the most beautiful and clearest coast along the Mediterranean. The city still preserves its importance as a center throughout history in the south coast of the country, in addition to its wonderful natural beauties. The mythological city which housed the Gods and Goddesses now exhibits all its secrets and marvels to mankind.",
+        imageUrl: "https://wallpaperaccess.com/full/1889144.jpg"
+    },
+    {   id:3,
+        title: "Istanbul",
+        location: "Turkey",
+        googleMapsUrl: "https://goo.gl/maps/xuMr7CDaET1jehFx7",
+        startDate: "4 Nov, 2022",
+        endDate: "12 Nov, 2022",
+        description: "İstanbul, the bustling metropolis of Türkiye, is a city full of charm, where the past goes hand in hand with the present and every step brings to light a relic of times gone by. At the crossroads of two continents - Europe and Asia - this spectacular city has absorbed the best of both worlds.With its beautiful historic landmarks, quintessential cuisine, vibrant nightlife, festivals of art and music, celebration of faith, labyrinths of marketplaces and shopping arcades, the beating heart of İstanbul, and the soul of Bosphorus under the watchful eye of the Maiden's Tower is a traveller's dream.",
+        imageUrl: "https://wallpapercave.com/wp/wp1813731.jpg"
+    }
+]
